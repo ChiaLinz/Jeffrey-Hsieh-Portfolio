97b7357c1bcd4fd4855c8f682f1fc5a565c70cdd
ChiaLinz <93101704+ChiaLinz@users.noreply.github.com>
2023-04-15 19:06:50 -0400

    update experience years

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -10,82 +10,82 @@ const Experience = () => {
         <h3 style={{ textAlign: 'center' }}>Programming Languages</h3>
         <div className="experience__content" style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)' }}>
           <article className="experience__details">
-            <h4>PythonL</h4>
+            <h4>Python ( 4+ years)</h4>
           </article>
           <article className="experience__details">
-            <h4>SQL</h4>
+            <h4>SQL ( 2+ years)</h4>
           </article>
           <article className="experience__details">
-            <h4>R</h4>
+            <h4>R ( 2+ years)</h4>
           </article>
           <article className="experience__details">
-            <h4>Java</h4>
+            <h4>HTML ( 2+ years)</h4>
           </article>
           <article className="experience__details">
-            <h4>JavaScript</h4>
+            <h4>CSS ( 2+ years)</h4>
           </article>
           <article className="experience__details">
-            <h4>HTML</h4>
+            <h4>Java ( 1+ year)</h4>
           </article>
           <article className="experience__details">
-            <h4>React.JS</h4>
+            <h4>JavaScript ( 1+ year)</h4>
           </article>
           <article className="experience__details">
-            <h4>CSS</h4>
+            <h4>React.JS ( 1+ year)</h4>
           </article>
         </div>
         <h3 style={{ textAlign: 'center' }}>Frameworks & Tools</h3>
         <div className="experience__content" style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)' }}>
           <article className="experience__details">
-            <h4>Power BI</h4>
+            <h4>Jupyter Notebook ( 4+ years)</h4>
           </article>
           <article className="experience__details">
-            <h4>Tableau</h4>
+            <h4>Scikit-learn ( 4+ years)</h4>
           </article>
           <article className="experience__details">
-            <h4>Jupyter Notebook</h4>
+            <h4>Git ( 4+ years)</h4>
           </article>
           <article className="experience__details">
-            <h4>Scikit-learn</h4>
+            <h4>TensorFlow ( 2+ years)</h4>
           </article>
           <article className="experience__details">
-            <h4>TensorFlow</h4>
+            <h4>Pytorch ( 2+ years)</h4>
           </article>
           <article className="experience__details">
-            <h4>Pytorch</h4>
+            <h4>Flask ( 2+ years)</h4>
           </article>
           <article className="experience__details">
-            <h4>Flask</h4>
+            <h4>Power BI ( 1+ year)</h4>
           </article>
           <article className="experience__details">
-            <h4>Git</h4>
+            <h4>Tableau ( 1+ year)</h4>
           </article>
         </div>
         <h3 style={{ textAlign: 'center' }}>Database & Data Warehouses</h3>
         <div className="experience__content" style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)' }}>
           <article className="experience__details">
-            <h4>MS SQL Server</h4>
+            <h4>MS SQL Server ( 2+ years)</h4>
           </article>
           <article className="experience__details">
-            <h4>Hive</h4>
+            <h4>Hive ( 1+ year)</h4>
           </article>
           <article className="experience__details">
-            <h4>Snowflake</h4>
+            <h4>Snowflake ( 1+ year)</h4>
           </article>
           <article className="experience__details">
-            <h4>Spark</h4>
+            <h4>Spark ( 1+ year)</h4>
           </article>
           <article className="experience__details">
-            <h4>MongoDB</h4>
+            <h4>MongoDB ( 1+ year)</h4>
           </article>
           <article className="experience__details">
-            <h4>IBM DB2</h4>
+            <h4>IBM DB2 ( 1+ year)</h4>
           </article>
           <article className="experience__details">
-            <h4>AWS</h4>
+            <h4>AWS ( 1+ year)</h4>
           </article>
           <article className="experience__details">
-            <h4>Azure</h4>
+            <h4>Azure ( 1+ year)</h4>
           </article>
         </div>
       </div>
